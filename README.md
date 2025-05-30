6a4a7a42c5d20994b737044862b8b840d0a31ecd
RigaudsonFrederic <frigaudson1997@gmail.com>
2025-05-30 14:19:09 -0400

    update

---
+++ README.md
@@ -0,0 +1,34 @@
+# Simple Todo App
+
+A simple and intuitive Todo application built with vanilla JavaScript using Object-Oriented Programming (OOP) principles. Users can add, complete, and delete tasks through a clean user interface.
+
+## Functionality
+
+- Render a list of predefined todos on page load.
+- Add new todos through a popup form.
+- Mark todos as completed using checkboxes.
+- Delete todos using a dedicated delete button.
+- Form includes input validation with real-time feedback.
+- Reset form inputs and disable submit button after successful submission.
+- Utilizes `uuid` to generate unique IDs for new todos.
+
+## Technology
+
+- HTML5
+- CSS3
+- JavaScript
+  - Modules
+  - Classes & OOP
+  - DOM Manipulation
+- Live Server for local development
+- Git & GitHub for version control and deployment
+
+### Features
+
+![Todo App Screenshot](./assets/todo-screenshot.png)
+
+## Deployment
+
+This project is deployed on GitHub Pages:
+
+- [🔗 Click here to view the live site](https://github.com/rfrederic/se_project_todo-app)
---
+++ pages/index.js
@@ -0,0 +1,82 @@
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
+
+import { initialTodos, validationConfig } from "../utils/constants.js";
+import Todo from "../Components/Todo.js";
+import FormValidator from "../Components/FormValidator.js";
+
+const addTodoButton = document.querySelector(".button_action_add");
+const addTodoPopup = document.querySelector("#add-todo-popup");
+const addTodoForm = addTodoPopup.querySelector(".popup__form");
+const addTodoCloseBtn = addTodoPopup.querySelector(".popup__close");
+const todoTemplate = document.querySelector("#todo-template");
+const todosList = document.querySelector(".todos__list");
+
+const openModal = (modal) => {
+  modal.classList.add("popup_visible");
+};
+
+const closeModal = (modal) => {
+  modal.classList.remove("popup_visible");
+};
+
+const generateTodo = (data) => {
+  const todo = new Todo(data, "#todo-template");
+  return todo.getview();
+  const todoNameEl = todoElement.querySelector(".todo__name");
+  const todoCheckboxEl = todoElement.querySelector(".todo__completed");
+  const todoLabel = todoElement.querySelector(".todo__label");
+  const todoDate = todoElement.querySelector(".todo__date");
+  const todoDeleteBtn = todoElement.querySelector(".todo__delete-btn");
+
+  todoNameEl.textContent = data.name;
+  todoCheckboxEl.checked = data.completed;
+
+  todoCheckboxEl.id = `todo-${data.id}`;
+  todoLabel.setAttribute("for", `todo-${data.id}`);
+
+  const dueDate = new Date(data.date);
+  if (!isNaN(dueDate)) {
+    todoDate.textContent = `Due: ${dueDate.toLocaleString("en-US", {
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    })}`;
+  }
+
+  todoDeleteBtn.addEventListener("click", () => {
+    todoElement.remove();
+  });
+
+  return todoElement;
+};
+
+addTodoButton.addEventListener("click", () => {
+  openModal(addTodoPopup);
+});
+
+addTodoCloseBtn.addEventListener("click", () => {
+  closeModal(addTodoPopup);
+});
+
+addTodoForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  const name = evt.target.name.value;
+  const dateInput = evt.target.date.value;
+
+  const date = new Date(dateInput);
+  date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
+
+  const id = uuidv4();
+  const values = { name, date, id };
+  const todo = generateTodo(values);
+  todosList.append(todo);
+  closeModal(addTodoPopup);
+});
+
+initialTodos.forEach((item) => {
+  const todo = generateTodo(item);
+  todosList.append(todo);
+});
+
+const newTodoValidator = new FormValidator(validationConfig, addTodoForm);
+newTodoValidator.enableValidation();
